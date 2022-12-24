1f19eb696f1f55f62dc644b01818f507d26ebe64
Nixon <nixonshowalter@yahoo.com>
2022-12-23 20:29:28 -0800

    Files created

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Type-Or-Py
-An automatic answer program for the game Type or Die on Roblox
+An automatic answer program for the game Type or Die on Roblox. All questions that are missing or incorrectly read are complied to missing_questions.txt to be added.
 
 
-I do not condone cheating
+I do not condone cheating.
 
 
-Tool only for use on private servers against consenting parties
+Tool only for use on private servers against consenting parties.
 
 I AM NOT RESPONSIBLE IF YOU GET BANNED USING THIS TOOL
---
+++ main.py
@@ -0,0 +1,53 @@
+from PIL import ImageGrab
+import pytesseract
+import csv
+import pyautogui
+from pynput import keyboard
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+def create_csv_dict(file_name):
+    with open(file_name, mode='r') as infile:
+        reader = csv.reader(infile)
+        answers_dict = {rows[0]:rows[1] for rows in reader}
+    return answers_dict
+
+
+def get_question():
+    question_image = ImageGrab.grab(bbox=(562,31,1356,100))
+    question_text = pytesseract.image_to_string(question_image)
+    return question_text[0:(len(question_text) - 1)]
+
+
+def type_answer(answer):
+    print(answer)
+    pyautogui.write(answer, 0.05)
+
+def get_answer():  
+    answers_dict = create_csv_dict('q&a.csv')
+    question = get_question()
+    if question in answers_dict or (question[0:7] in "NAME A " and question[7:] in answers_dict):
+        if question[0:7] in "NAME A " and question[7:] in answers_dict:
+            type_answer(answers_dict[question[7:]])
+        else:
+            type_answer(answers_dict[question])
+    else:
+        print("No answer for the question: " + question )
+        with open('missing_questions.txt', 'a') as file:
+            file.write(question + "\n")
+        print(question + " has been added to the list of missing questions.")
+
+
+def on_press(key):
+    if key == keyboard.Key.esc:
+        return False  
+    try:
+        k = key.char 
+    except:
+        k = key.name  
+    if k in ['f8']: 
+        print("Getting answer...")
+        get_answer()
+
+listener = keyboard.Listener(on_press)
+listener.start()
+listener.join() 
